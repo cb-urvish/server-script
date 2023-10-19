3c010c92ae6efab3caac09a3f1af6985b3931cb6
cb-urvish <urvish.r@chipbucket.com>
2023-10-19 18:04:46 +0530

    fix bug

--- server.js
+++ server.js
@@ -9,18 +9,5 @@ client.connect(8989, '192.168.1.79', function() {
 client.on('data', function(data) {
     const receivedData = data.toString().trim();
     console.log('Received: ' + receivedData);
-
-    // Send the received data to the Homebridge plugin
-    sendToHomebridgePlugin(receivedData);
 });
 
-function sendToHomebridgePlugin(data) {
-    // Set up a connection to the Homebridge plugin's IP and port
-    const homebridgePluginClient = new net.Socket();
-	setTimeout(() => {
-		homebridgePluginClient.connect(9000, '127.0.0.1', function() {
-			console.log('Connected');
-			homebridgePluginClient.write('Hello, server! Love, Client.');
-		});
-	}, 5000); 
-}
