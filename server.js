4e53e46e4a1bc5d27466b731b3096ca845518c25
cb-urvish <urvish.r@chipbucket.com>
2023-10-19 17:33:14 +0530

    fix bug

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-let net = require('net');
-let client = new net.Socket();
+const net = require('net');
+const client = new net.Socket();
 
 client.connect(8989, '192.168.1.79', function() {
-	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+    console.log('Connected');
+    client.write('Hello, server! Love, Client.');
 });
 
 client.on('data', function(data) {
-	console.log('Received: ' + data);
+    const receivedData = data.toString().trim();
+    console.log('Received: ' + receivedData);
+
+    // Send the received data to the Homebridge plugin
+    sendToHomebridgePlugin(receivedData);
 });
 
-client.on("error", (error) => {
-    console.log(`Socket Error: ${error.message}`);
-});
-
-// client.on('close', function() {
-// 	console.log('Connection closed');
-// });
-
+function sendToHomebridgePlugin(data) {
+    // Set up a connection to the Homebridge plugin's IP and port
+    const homebridgePluginClient = new net.Socket();
+	setTimeout(() => {
+		homebridgePluginClient.connect(9000, '127.0.0.1', function() {
+			console.log('Connected');
+			homebridgePluginClient.write('Hello, server! Love, Client.');
+		});
+	}, 5000); 
+}
